cc19ca437fec1141f5380ceb8c72523320f89e03
Ahrav Dutta <ahrav.dutta@trufflesec.com>
2024-12-18 10:07:53 -0800

    fix

--- src/config.h
+++ src/config.h
@@ -13,67 +13,67 @@
 /* Allowlist structure */
 typedef struct
 {
-    char *description;
+	char *description;
 
-    char *regex_target;
+	char *regex_target;
 
-    char **regexes;
-    OnigRegex **compiled_regexes;
-    int num_regexes;
+	char **regexes;
+	OnigRegex **compiled_regexes;
+	int num_regexes;
 
-    char **stopwords;
-    int num_stopwords;
+	char **stopwords;
+	int num_stopwords;
 
-    char **paths;
-    int num_paths;
+	char **paths;
+	int num_paths;
 } allowlist_t;
 
 /* Rule structure */
 typedef struct
 {
-    char *id;
-    char *description;
-    char *regex;
-    OnigRegex compiled_regex;
-    char **keywords;
-    char **stopwords;
-    float entropy;
-    int num_keywords;
-    int num_stopwords;
-    int ref_count; // Reference count
-    allowlist_t *allowlist;
+	char *id;
+	char *description;
+	char *regex;
+	OnigRegex compiled_regex;
+	char **keywords;
+	char **stopwords;
+	float entropy;
+	int num_keywords;
+	int num_stopwords;
+	int ref_count; // Reference count
+	allowlist_t *allowlist;
 
 } Rule;
 
 /* A structure to hold a list of rules associated with a keyword */
 typedef struct rule_list
 {
-    Rule *rule;
-    struct rule_list *next;
+	Rule *rule;
+	struct rule_list *next;
 } rule_list;
 
 typedef struct keyword_hash
 {
-    char *keyword;     // Key: the keyword
-    rule_list *rules;  // List of rules associated with this keyword
-    UT_hash_handle hh; // makes this structure hashable
+	char *keyword;		 // Key: the keyword
+	rule_list *rules;	 // List of rules associated with this keyword
+	UT_hash_handle hh; // makes this structure hashable
 } keyword_hash;
 
 /* Config_t structure */
 typedef struct
 {
-    /* Hashmap of rules for quick lookup */
-    keyword_hash *rule_table;
+	/* Hashmap of rules for quick lookup */
+	keyword_hash *rule_table;
 
-    /* Dictionary of keywords for Aho-Corasick */
-    unsigned char **dictionary;
-    int num_words_in_dictionary;
+	/* Dictionary of keywords for Aho-Corasick */
+	unsigned char **dictionary;
+	int num_words_in_dictionary;
 
-    /* Aho-Corasick root node */
-    AhoCorasick *aho;
+	/* Aho-Corasick root node */
+	AhoCorasick *aho;
 
-    /* TODO Global Allowlist */
-    allowlist_t *allowlist;
+	/* TODO Global Allowlist */
+	allowlist_t *allowlist;
 
 } Config_t;
 
